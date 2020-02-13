d220ff8a419f99050287f5fefbaedcc493bf1348
SebastianMelin <57399859+SebastianMelin@users.noreply.github.com>
2020-02-13 10:35:45 +0100

    Add project files.

---
+++ TypeLib/IRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TypeLib;
+
+namespace TypeLib
+{
+    public interface IRepository : IDisposable
+    {
+     
+        Task<Beverage> AddBeverage(Beverage beverage);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Ingredient> AddIngredient(Ingredient ingredient);
+        Task<Pasta> AddPasta(Pasta pasta);
+        Task<PizzaRecipe> AddPizza(PizzaRecipe pizza);
+        Task<Sallad> AddSallad(Sallad sallad);
+        Task<Beverage> DeleteBeverage(Beverage beverage);
+        Task<Employee> DeleteEmployee(Employee employee);
+        Task<Ingredient> DeleteIngredient(Ingredient ingredient);
+        Task<Pasta> DeletePasta(Pasta pasta);
+        Task<PizzaRecipe> DeletePizza(PizzaRecipe pizza);
+        Task<Sallad> DeleteSallad(Sallad sallad);
+        Task<Beverage> EditBeverage(Beverage beverage);
+        Task<Employee> EditEmployee(Employee employee);
+        Task<Ingredient> EditIngredient(Ingredient ingredient);
+        Task<Pasta> EditPasta(Pasta pasta);
+        Task<PizzaRecipe> EditPizza(PizzaRecipe pizza);
+        Task<Sallad> EditSallad(Sallad sallad);
+        Task<IEnumerable<Beverage>> GetBeverages();
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<IEnumerable<Ingredient>> GetIngredients();
+        Task<IEnumerable<Pasta>> GetPastas();
+        Task<IEnumerable<PizzaRecipe>> GetPizzas();
+        Task<IEnumerable<Sallad>> GetSallads();
+        Task<Order> DeleteOrders(Order order);
+        Task<IEnumerable<Order>> GetOrdersMaking();
+    }
+}
---
+++ TypeLib/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TypeLib
+{
+    public class PizzaRecipe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
